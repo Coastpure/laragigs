2b57e14910e7d2101569ac9f2f1b0c55d9b07597
Coastpure <njorogesimon465@gmail.com>
2023-02-07 15:27:26 +0300

    Done

--- resources/views/partials/_hero.blade.php
+++ resources/views/partials/_hero.blade.php
@@ -1,7 +1,6 @@
 
 <section
-class="relative w-full h-48 flex flex-col justify-center align-center text-center space-y-4 mb-4"
-style="background-image: url('images/hero2.jpg')"
+class="relative bg-laravel w-full h-48 flex flex-col justify-center align-center text-center space-y-4 mb-4"
 >
 <div
     class="absolute top-0 left-0 w-full h-full opacity-10 bg-no-repeat bg-center"
